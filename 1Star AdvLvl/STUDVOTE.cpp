8280235286cb7c2f856573b5048a1ad336669e38
Arindal Char <110285827+arindal1@users.noreply.github.com>
2023-07-20 09:33:44 +0530

    Update STUDVOTE.cpp

--- 1Star AdvLvl/STUDVOTE.cpp
+++ 1Star AdvLvl/STUDVOTE.cpp
@@ -4,14 +4,24 @@ using namespace std;
 void vote() {
     int n,k;
     cin >> n >> k;
-    int arr[n]={0};
-    int c=0;
+    int arr[n+1]={0};
+    int c=0, dis=0;
+    
     for(int i=1; i<=n; i++) {
         int a;
         cin >> a;
-        arr[a]++;
-        if(arr[a]==k && a!=i)
-            c++;
+        if(a!=i && a!=dis) {
+            arr[a]++;
+        }
+        else {
+            arr[a]=0;
+            continue;
+            dis=a;
+        }
+        for(int i=1; i<=n; i++) {
+            if(arr[i]>=k)
+                c++;
+        }
     }
     cout << c << endl;
 }
@@ -22,6 +32,43 @@ int main() {
 	cin >> t;
 	while(t--) {
 	    vote();
+	}
+	return 0;
+}
+
+//////
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,k;
+	    cin>>n>>k;
+	    int arr[n];
+	    int count=0;
+	    int votes[n]={0};
+	    
+	    for(int i=0;i<n;i++){
+	        cin>>arr[i];
+	        int p = arr[i];
+	        if (p==i+1)
+	        votes[p-1]=-1;
+	        else {
+	            if (votes[p-1]!=-1)
+	            votes[p-1]++;
+	            
+	        }
+	        
+	    }
+	    for(int i=0; i<n;i++){
+	        if((votes[i]>k)||(votes[i]==k))
+	        count++;
+	    }
+	    cout<<count<<endl;
 	}
 	return 0;
 }
