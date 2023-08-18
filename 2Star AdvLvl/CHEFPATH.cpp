cba47f36437505372cb05685f95a4a23c4ab7e8d
Arindal Char <110285827+arindal1@users.noreply.github.com>
2023-08-18 08:36:37 +0530

    Create CHEFPATH.cpp

---
+++ 2Star AdvLvl/CHEFPATH.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void magicalPath() {
+    long long int a, b;
+    cin >> a >> b;
+    if (a==1&&b==2||a==2&&b==1)
+        cout << "YES" << endl;
+    else if (a==1||b==1)
+        cout << "NO" << endl;
+    else if (a%2!=0&&b%2!=0)
+        cout << "NO" << endl;
+    else 
+        cout << "YES" << endl;
+}
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while (t--) {
+	    magicalPath();
+	}
+	return 0;
+}
