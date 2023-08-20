0fa7f0dfcb07b611e2b87a2f0a0e50a443b77284
Arindal Char <110285827+arindal1@users.noreply.github.com>
2023-08-20 07:32:07 +0530

    Create HP18.cpp

---
+++ 2Star AdvLvl/HP18.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+void luckyNumber() {
+    int n, a, b;
+    cin >> n >> a >> b;
+    int arr[n];
+    int alice = 0, bob = 0, albob = 0;
+    
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+        if(arr[i] % a == 0 && arr[i] % b == 0) {
+            albob++;
+        }
+        else if(arr[i] % a == 0) {
+            bob++;
+        }
+        else if(arr[i] % b == 0) {
+            alice++;
+        }
+    }
+    if(alice == 0 && bob == 0 && albob != 0)
+        cout << "BOB" << endl;
+    
+    else if(alice == 0 && bob == 0 && albob == 0)
+        cout << "ALICE" << endl;
+    
+    else if(albob == 0) {
+        if(alice >= bob)
+            cout << "ALICE" << endl;
+        else
+            cout << "BOB" << endl;
+    }
+    else {
+        if(alice > bob) 
+            cout << "ALICE" << endl;
+        else
+            cout << "BOB" << endl;
+    }
+}
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while (t--) {
+	    luckyNumber();
+	}
+	return 0;
+}
