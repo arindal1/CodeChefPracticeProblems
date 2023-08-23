bd0690b266a721ad2973b4c2de03fc8dd9c67389
Arindal Char <110285827+arindal1@users.noreply.github.com>
2023-08-23 11:08:25 +0530

    Create REDONE.py

---
+++ 2Star AdvLvl/REDONE.py
@@ -0,0 +1,7 @@
+mod = (10 ** 9) + 7 
+l = [0]
+for t in range(int(input())):
+    n = int(input())
+    for i in range(len(l), n + 1):
+        l.append(((l[i - 1] * (i + 1)) + i) % mod)
+    print(l[n])
