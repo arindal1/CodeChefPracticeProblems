cb1a1c65704bb76508f75a85d7cc9c8be85ca309
Arindal Char <110285827+arindal1@users.noreply.github.com>
2023-09-06 20:20:21 +0530

    Create LUCKYSEVEN.cpp

---
+++ Contest Problems/LUCKYSEVEN.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	string S;
+    cin >> S;
+    char luckyLetter = S[6];
+
+    cout << luckyLetter << endl;
+	return 0;
+}
